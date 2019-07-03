aae726c3137fe24ba2448964320ccf1a964f2dcf
Chin-Ming Huang <chinming@writepath.co>
2019-07-03 15:25:25 +0800

    Add region for aws config

--- travis/credentials.sh
+++ travis/credentials.sh
@@ -2,8 +2,14 @@
 
 mkdir -p ~/.aws
 
-cat > ~/.aws/credentials << EOL
+cat > ~/.aws/credentials << END_OF_CREDENTIALS
 [default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
-EOL
+END_OF_CREDENTIALS
+
+cat > ~/.aws/config << END_OF_CONFIG
+[default]
+region=ap-northeast-1
+output=json
+END_OF_CONFIG
