3780a72ceb6cf67d3c8a4fa0f9a10473f1ae5c5a
Chin-Ming Huang <chinming@writepath.co>
2019-07-04 12:15:19 +0800

    Update

--- travis/deploy_eb.sh
+++ travis/deploy_eb.sh
@@ -10,13 +10,15 @@ echo "Zip and upload Dockerrun file"
 zip -r $ZIP . -i Dockerrun.aws.json
 aws s3 cp $ZIP s3://$EB_BUCKET/$ZIP
 
-echo "Create a new app version"
-aws elasticbeanstalk create-application-version \
-    --application-name $AWS_EB_APP_NAME \
-    --version-label $VERSION \
-    --source-bundle S3Bucket=$EB_BUCKET,S3Key=$ZIP
+eb deploy ${AWS_EB_APP_ENV}
 
-echo "Update app environment"
-aws elasticbeanstalk update-environment \
-    --environment-name $AWS_EB_APP_ENV \
-    --version-label $VERSION
+#echo "Create a new app version"
+#aws elasticbeanstalk create-application-version \
+#    --application-name $AWS_EB_APP_NAME \
+#    --version-label $VERSION \
+#    --source-bundle S3Bucket=$EB_BUCKET,S3Key=$ZIP
+
+#echo "Update app environment"
+#aws elasticbeanstalk update-environment \
+#    --environment-name $AWS_EB_APP_ENV \
+#    --version-label $VERSION
