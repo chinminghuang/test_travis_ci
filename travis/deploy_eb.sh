da7557e78538c99fbbb3e22095aacc9a30e726e2
Chin-Ming Huang <chinming@writepath.co>
2019-07-04 14:49:32 +0800

    No ZIP

--- travis/deploy_eb.sh
+++ travis/deploy_eb.sh
@@ -7,16 +7,19 @@ EB_BUCKET="elasticbeanstalk-ap-northeast-1-889231634371"
 ZIP="$AWS_EB_APP_NAME.$VERSION.zip"
 
 echo "Zip and upload Dockerrun file"
-zip -r $ZIP . -i Dockerrun.aws.json
-aws s3 cp $ZIP s3://$EB_BUCKET/$ZIP
+#zip -r $ZIP . -i Dockerrun.aws.json
+#aws s3 cp $ZIP s3://$EB_BUCKET/$ZIP
+aws s3 cp Dockerrun.aws.json s3://$EB_BUCKET/$ZIP
 
 echo "Create a new app version"
 aws elasticbeanstalk create-application-version \
+    --region ap-northeast-1 \
     --application-name $AWS_EB_APP_NAME \
     --version-label $VERSION \
     --source-bundle S3Bucket=$EB_BUCKET,S3Key=$ZIP
 
 echo "Update app environment"
 aws elasticbeanstalk update-environment \
+    --region ap-northeast-1 \
     --environment-name $AWS_EB_APP_ENV \
     --version-label $VERSION
